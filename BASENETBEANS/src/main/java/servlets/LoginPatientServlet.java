93b350b540f15985ec00cc74d851fc313b0c2c6a
Manos Lourakis <lourakismanos@gmail.com>
2021-01-07 16:46:41 +0200

    Fixed some warnings

--- BASENETBEANS/src/main/java/servlets/LoginPatientServlet.java
+++ BASENETBEANS/src/main/java/servlets/LoginPatientServlet.java
@@ -8,7 +8,6 @@ package servlets;
 import com.google.gson.Gson;
 import db.CS360DB;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,7 +20,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.Patient;
 
 /**
  *
@@ -37,66 +35,57 @@ public class LoginPatientServlet extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     * @throws java.lang.ClassNotFoundException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException, ClassNotFoundException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
 
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
 
-            boolean correct = false; //if the patient exists
-            Connection con = CS360DB.getConnection();
-            Statement stmt = con.createStatement();
-            String info = request.getParameter("amka");
-            int amka = Integer.parseInt(info);
+        boolean correct = false; //if the patient exists
+        Connection con = CS360DB.getConnection();
+        Statement stmt = con.createStatement();
+        String info = request.getParameter("amka");
+        int amka = Integer.parseInt(info);
 
-            System.out.println("ENTER THE SERVLET");
+        System.out.println("ENTER THE SERVLET");
 
-            String insQuery = new String("SELECT * FROM `Patient` WHERE `AMKA` = " + amka);
-            ResultSet rs = stmt.executeQuery(insQuery);
-            Patient patient;
-            patient = new Patient();
-            Map map = new HashMap();
-            if (rs.next()) {
-                System.out.println("Found him");
-                map.put("Exists", true);
-                map.put("AMKA", rs.getInt("AMKA"));
-                map.put("First_Name", rs.getString("First_Name"));
-                map.put("Last_Name", rs.getString("Last_Name"));
-                map.put("Address", rs.getString("Address"));
-                map.put("Insurance", rs.getString("Insurance"));
-                map.put("Phone", rs.getString("Phone"));
-                map.put("PID", rs.getInt("PID"));
+        String insQuery = "SELECT * FROM `Patient` WHERE `AMKA` = " + amka;
+        ResultSet rs = stmt.executeQuery(insQuery);
+        Map map = new HashMap();
+        if (rs.next()) {
+            System.out.println("Found him");
+            map.put("Exists", true);
+            map.put("AMKA", rs.getInt("AMKA"));
+            map.put("First_Name", rs.getString("First_Name"));
+            map.put("Last_Name", rs.getString("Last_Name"));
+            map.put("Address", rs.getString("Address"));
+            map.put("Insurance", rs.getString("Insurance"));
+            map.put("Phone", rs.getString("Phone"));
+            map.put("PID", rs.getInt("PID"));
+        } else {
+            map.put("Exists", false);
+        }
 
+        insQuery = "SELECT * FROM `Previous visit` WHERE `AMKA` = " + amka;
+        rs = stmt.executeQuery(insQuery);
 
-            } else {
-                map.put("Exists", false);
-            }
-
-            insQuery = new String("SELECT * FROM `Previous visit` WHERE `AMKA` = " + amka);
-            rs = stmt.executeQuery(insQuery);
-
-            if (rs.next()) {
-                map.put("Has_Prev_visit", true);
-                map.put("AMKA", rs.getInt("AMKA"));
-                map.put("ExaminationID", rs.getString("ExaminationID"));
-                map.put("Date", rs.getString("Date"));
-                map.put("Diagnosis", rs.getString("Diagnosis"));
-                map.put("Examination", rs.getString("Examination"));
-                map.put("Cure", rs.getString("Cure"));
-            }
-            else
-                map.put("Has_Prev_visit", false);
+        if (rs.next()) {
+            map.put("Has_Prev_visit", true);
+            map.put("AMKA", rs.getInt("AMKA"));
+            map.put("ExaminationID", rs.getString("ExaminationID"));
+            map.put("Date", rs.getString("Date"));
+            map.put("Diagnosis", rs.getString("Diagnosis"));
+            map.put("Examination", rs.getString("Examination"));
+            map.put("Cure", rs.getString("Cure"));
+        } else {
+            map.put("Has_Prev_visit", false);
 
             Gson gson = new Gson();
             String json = gson.toJson(map);
             response.getWriter().write(json);
-
-
-
         }
     }
 
